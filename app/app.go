660e67a5f719c72beaf980b9ce16142c66455b8c
Dudu <dudsjrsilva@gmail.com>
2022-01-04 10:24:14 -0300

    add getNames function

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Generate() *cli.App {
 	app.Name = "Get IP"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: getIPs,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca nome dos servidores na internet",
+			Flags:  flags,
+			Action: getNames,
 		},
 	}
 
@@ -41,5 +49,19 @@ func getIPs(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func getNames(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
